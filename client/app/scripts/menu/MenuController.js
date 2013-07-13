6cc971901f678cebd1db6ae02506d0ea08b48bdd
Pablo De Nadai <pablodenadai@gmail.com>
2013-07-14 00:30:55 +1000

    - New menu items.
    - Add Search.
    - Add New Match and Venue.
    - Add Settings.

--- client/app/scripts/menu/MenuController.js
+++ client/app/scripts/menu/MenuController.js
@@ -4,8 +4,20 @@ app.controller('MenuCtrl', function ($scope, AuthenticationModel) {
 
 	$scope.AuthenticationModel = AuthenticationModel;
 
-	$scope.signOut = function(){
+	$scope.search = null;
+	$scope.isSearching = false;
+
+	$scope.signOut = function () {
 		AuthenticationModel.signOut();
+	};
+
+	$scope.stopSearching = function () {
+		$scope.search = '';
+		$scope.isSearching = false;
+	};
+
+	$scope.startSearching = function () {
+		$scope.isSearching = true;
 	};
 
 });

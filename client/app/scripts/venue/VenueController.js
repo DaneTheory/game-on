8b4919dd1b5cf28c60c8bc7bb26d200031cfa3f4
Pablo De Nadai <pablodenadai@gmail.com>
2013-07-14 01:12:17 +1000

    - Implemented Map directive.

--- client/app/scripts/venue/VenueController.js
+++ client/app/scripts/venue/VenueController.js
@@ -10,9 +10,7 @@ app.controller('VenueCtrl', function ($scope, $routeParams, $location, VenueMode
 	$scope.maxDistance = 100; // Km
 
 	$scope.getById = function () {
-		VenueModel.getById($scope.venueId).success(function(){
-			map();
-		});
+		VenueModel.getById($scope.venueId);
 	};
 
 	$scope.getCollection = function () {
@@ -30,32 +28,10 @@ app.controller('VenueCtrl', function ($scope, $routeParams, $location, VenueMode
 		);
 	};
 
-	var map = function () {
-		var position = new google.maps.LatLng(
-			VenueModel.venue.location[0], 
-			VenueModel.venue.location[1]);
-
-		var mapOptions = {
-			zoom: 14,
-			disableDefaultUI: true,
-    		center: position,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		
-		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-		var marker = new google.maps.Marker({
-			position: position,
-			map: map,
-			title: VenueModel.venue.name
-		});
-
-		return true;
-	}
-
 	var getGeoLocationSuccess = function(location) {
 		// See https://groups.google.com/forum/?fromgroups#!topic/angular/nFbtADyEHg8
 		$scope.$apply(function() {
+			console.log(location.coords);
 			$location.search({
 				latitude: location.coords.latitude,
 				longitude: location.coords.longitude

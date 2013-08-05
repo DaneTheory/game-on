8fe6adfb6695e7d915fb78b1a85c37ccd9debf53
Pablo De Nadai <pablodenadai@gmail.com>
2013-08-05 21:05:39 +1000

    Open up project to public
    - JSHint warnings fix.
    - Remove Facebook API key.
    - Remove jQuery and Boostrap JS files.

--- server/app/env.js
+++ server/app/env.js
@@ -1,8 +1,8 @@
 module.exports = function() {
 	var env = {
 		'development': {
-			'facebook-oauth-key': '359014320866601',
-			'facebook-oauth-secret': 'dbd46c18e7cf3b71099d43ad4b08a3e4',
+			'facebook-oauth-key': '',
+			'facebook-oauth-secret': '',
 
 			'client-url-cors': 'http://localhost:9000'
 		}, 

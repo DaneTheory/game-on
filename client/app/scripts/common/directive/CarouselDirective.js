f6c916ea1fe372d5791db7320a80b22efbc77620
Pablo De Nadai <pablodenadai@gmail.com>
2013-12-07 01:16:05 +1100

    Background slider now updates the profile

--- client/app/scripts/common/directive/CarouselDirective.js
+++ client/app/scripts/common/directive/CarouselDirective.js
@@ -6,14 +6,27 @@
 
 app.directive('carousel', function () {
     return {
+    	scope: {
+    		model: '='
+    	},
         restrict: 'A',
-        link: function (scope, element) {
+        link: function (scope, element, attrs) {
 
+        	//
+        	//
+        	//
         	element.find('.carousel-control').on('click', function (e) {
         		var slideDirection = angular.element(e.currentTarget).data('slide');
         		element.carousel(slideDirection);
-        		element.carousel('pause');
         	});
+
+        	element.on('slid.bs.carousel', function () {
+        		var data = angular.element(this).data('bs.carousel'),
+        			index = data.getActiveIndex();
+
+				scope.model = index;
+				scope.$apply();
+			});
 
         }
     };

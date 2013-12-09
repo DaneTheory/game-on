6a4b849f182d1ef49fe4f5a43071f397af234ec5
Pablo De Nadai <pablodenadai@gmail.com>
2013-12-09 20:11:14 +1100

    Implement test coverage

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -36,6 +36,22 @@ module.exports = function(config) {
       'test/spec/**/*.js'
     ],
 
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'app/scripts/**/*.js': ['coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
+
     // list of files / patterns to exclude
     exclude: [],
 
@@ -46,10 +62,8 @@ module.exports = function(config) {
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // Start these browsers, currently available:
     // - Chrome
@@ -60,7 +74,6 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     browsers: ['Chrome'],
-
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
